0ed430cd731118487b390338cd72014dbef40759
kremnev8 <kremnev8super@yandex.ru>
2021-02-10 13:27:35 +0300

    Added Fix Prefabs script

---
+++ Unity/Editor/Baker/FixPrefabMeshes.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace UnityEditor
+{
+    public class FixPrefabMeshes
+    {
+        [MenuItem("Window/DSP Tools/Fix prefab(Can crash unity!)")]
+        public static void FixPrefabs()
+        {
+            RemoveMissingScripts();
+            
+            GameObject gameObject = Selection.activeGameObject;
+
+            if (gameObject == null)
+            {
+                Debug.Log("You have nothing selected!");
+                return;
+            }
+
+            MeshFilter[] filters = gameObject.GetComponentsInChildren<MeshFilter>();
+
+            foreach (MeshFilter filter in filters)
+            {
+                if (filter.sharedMesh == null)
+                {
+                    string[] found = AssetDatabase.FindAssets(filter.gameObject.name + " t:mesh");
+                    if (found.Length > 0)
+                    {
+                        Debug.Log("Found mesh with name: " + AssetDatabase.GUIDToAssetPath(found[0]));
+                        //Mesh mesh = (Mesh) Resources.Load(AssetDatabase.GUIDToAssetPath(found[0]), typeof(Mesh));
+
+                        Mesh mesh = AssetDatabase.LoadAssetAtPath<Mesh>(AssetDatabase.GUIDToAssetPath(found[0]));
+
+                        filter.sharedMesh = mesh;
+                        
+                        UnityEditor.EditorUtility.SetDirty(filter.gameObject);
+                    }
+                }
+            }
+        }
+
+        
+        private static void DoIterate(GameObject gameObject)
+        {
+            foreach (Transform child in gameObject.transform)
+            {
+                handleChild(child.gameObject);
+                
+                DoIterate(child.gameObject);
+            }
+        }
+
+        private static void handleChild(GameObject gameObject)
+        {
+            var components = gameObject.GetComponents<Component>();
+         
+            // Create a serialized object so that we can edit the component list
+            var serializedObject = new SerializedObject(gameObject);
+            // Find the component list property
+            var prop = serializedObject.FindProperty("m_Component");
+         
+            int r = 0;
+         
+            for(int j = 0; j < components.Length; j++)
+            {
+                if(components[j] == null)
+                {
+                    prop.DeleteArrayElementAtIndex(j-r);
+                    r++;
+                }
+            }
+         
+            // Apply our changes to the game object
+            serializedObject.ApplyModifiedProperties();
+            UnityEditor.EditorUtility.SetDirty(gameObject);
+        }
+        
+        private static void RemoveMissingScripts()
+        {
+            GameObject target = Selection.activeGameObject;
+
+            Queue<GameObject> queue = new Queue<GameObject>();
+            queue.Enqueue(target);
+
+            while (queue.Count > 0)
+            {
+                GameObject gameObject = queue.Dequeue();
+                handleChild(gameObject);
+                foreach (Transform child in gameObject.transform)
+                {
+                    queue.Enqueue(child.gameObject);
+                }
+            }
+            
+        }
+    }
+}
