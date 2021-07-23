e934c27dcd7718d7e3a905805a4ed4df1b450d35
kremnev8 <kremnev8super@yandex.ru>
2021-07-23 21:49:33 +0300

    Upload BlueprintTweaks source code

---
+++ Mods/BlueprintTweaks/BlueprintTweaksPlugin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+using System.Security;
+using System.Security.Permissions;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+[module: UnverifiableCode]
+#pragma warning disable 618
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore 618
+
+namespace BlueprintTweaks
+{
+    [BepInPlugin(MAINGUID, MAINNAME, VERSION)]
+    public class BlueprintTweaksPlugin : BaseUnityPlugin
+    {
+        public const string MODID = "BlueprintTweaks";
+        public const string MAINGUID = "org.kremnev8.plugin." + MODID;
+        public const string MAINNAME = "Blueprint Tweaks";
+        
+        public const string VERSION = "1.0.0";
+
+        public static ManualLogSource logger;
+
+        private void Awake()
+        {
+            logger = Logger;
+            
+            Harmony harmony = new Harmony(MAINGUID);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            
+            logger.LogInfo("Blueprint tweaks mod is initialized!");
+        }
+
+        private void Update()
+        {
+            if (KeyBindPatch.toggleBpMode)
+            {
+                CameraFixPatch.mode = !CameraFixPatch.mode;
+            }
+        }
+    }
+}
