0fc6f743d9d82a831236706045a250a26f948f05
kremnev8 <kremnev8super@yandex.ru>
2022-02-16 21:13:21 +0300

    Fix one file missing in GigaStations source code

---
+++ Mods/GigaStations/BlueprintBuilding_Patch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+
+namespace GigaStations
+{
+    [HarmonyPatch]
+    public static class BlueprintBuilding_Patch
+    {
+        [HarmonyPatch(typeof(BlueprintBuilding), nameof(BlueprintBuilding.Import))]
+        [HarmonyPostfix]
+        public static void UpdateBlueprintData(BlueprintBuilding __instance)
+        {
+            if (__instance.itemId != GigaStationsPlugin.ils.ID && 
+                __instance.itemId != GigaStationsPlugin.pls.ID && 
+                __instance.itemId != GigaStationsPlugin.collector.ID) // not my stations
+            {
+                return;
+            }
+
+            ItemProto proto = LDB.items.Select(__instance.itemId);
+            __instance.modelIndex = (short)proto.prefabDesc.modelIndex;
+        }
+    }
+}
